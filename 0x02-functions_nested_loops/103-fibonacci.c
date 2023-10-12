d9c83f5a9dc4fcf6f48e4f387c515c0982f5ee45
Frank45678 <franklinikeh20@gmail.com>
2023-10-11 23:41:09 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include 'main.h"
 
 /**
   * main - the main printing funtion
@@ -10,19 +10,24 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	unsigned long fib1 = 0. fib2 = 1. sum;
+	float total_sum;
 
-
-	while (k + j < 4000000)
+	while (1)
 	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
 	}
-	printf("%ld", sum);
+	printf("%.0f\n", total_sum);
+
 	return (0);
 }
 
